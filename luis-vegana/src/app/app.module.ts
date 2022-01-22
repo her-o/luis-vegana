c7270717c07d875dd13a4dd4396573473d6dcdd3
Hernan <hernan.olmos11@gmail.com>
2022-01-22 16:03:20 -0300

    hero-section

---
+++ luis-vegana/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroComponent } from './components/sections/hero/hero.component';
+import { RecipesComponent } from './components/sections/recipes/recipes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    RecipesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
